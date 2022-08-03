11979eb5ec26b027a6936a0429f92dc2747e44e8
MZC01-RYUMUN <MZC01-RYUMUN@MZC01-RYUMUN>
2022-08-03 15:12:15 +0900

    .

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -2,15 +2,22 @@ package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.example.demo.controller.LogController;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class DemoApplicationTests {
+	
+	@Autowired
+	LogController log;
 
 	@Test
 	public void contextLoads() {
+		log.test();
 	}
 
 }
